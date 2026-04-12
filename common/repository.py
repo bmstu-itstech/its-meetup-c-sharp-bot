c2e3ded28166b9ee6a1c64c8ff7d746b27d3c784
Elkin Kirill <kirillv.2016@mail.ru>
2026-04-12 23:40:07 +0300

    chore: add proxy, update docker and github deploy

--- common/repository.py
+++ common/repository.py
@@ -3,6 +3,11 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from config import config
 
+proxy_url = None
+if all([config.proxy_protocol, config.proxy_login,
+        config.proxy_password, config.proxy_ip,
+        config.proxy_port]):
+    proxy_url = f"{config.proxy_protocol}://{config.proxy_login}:{config.proxy_password}@{config.proxy_ip}:{config.proxy_port}"
 
-bot = Bot(token=config.telegram_bot_token)
+bot = Bot(token=config.telegram_bot_token, proxy=proxy_url)
 dp = Dispatcher(bot, storage=MemoryStorage())
